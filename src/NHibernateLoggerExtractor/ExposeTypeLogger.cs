1c4c632c21d778d2109a1f99c0446f4ef1877c29
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-03-23 15:16:05 +0200

    Fix nhibernate custom logger configuration

---
+++ src/NHibernateLoggerExtractor/ExposeTypeLogger.cs
@@ -0,0 +1,14 @@
+﻿using NHibernate;
+
+namespace NHibernateLoggerExtractor
+{
+    public class ExposeTypeLogger : NoLoggingInternalLogger
+    {
+        public string Logger { get; set; }
+
+        public ExposeTypeLogger(string logger)
+        {
+            Logger = logger;
+        }
+    }
+}
--- src/NHibernateLoggerExtractor/ExposeTypeLoggerFactory.cs
+++ src/NHibernateLoggerExtractor/ExposeTypeLoggerFactory.cs
@@ -3,16 +3,6 @@ using NHibernate;
 
 namespace NHibernateLoggerExtractor
 {
-    public class ExposeTypeLogger : NoLoggingInternalLogger
-    {
-        public string Logger { get; set; }
-
-        public ExposeTypeLogger(string logger)
-        {
-            Logger = logger;
-        }
-    }
-
     public class ExposeTypeLoggerFactory : ILoggerFactory
     {
         public IInternalLogger LoggerFor(string keyName)
